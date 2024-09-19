14afcf84f47811b8b71b4068a6a15e1e65854028
Juan8516 <juan.ind85@gmail.com>
2024-09-19 14:38:49 -0500

    feat: uso de contructor

--- 03POO/01class.php
+++ 03POO/01class.php
@@ -1,20 +1,21 @@
 <?php
 
-/*Una clase es una plantilla que define propiedades y métodos para crear objetos, permitiendo organizar y 
-reutilizar código mediante la programación orientada a objetos (POO)*/
+/*Una clase es una plantilla que define propiedades y métodos para crear objetos, 
+permitiendo organizar y reutilizar código mediante la programación orientada a objetos (POO)*/
 
-// Creación de una nueva instancia de la clase Sale
-$sale = new Sale();
+// Creación de una nueva instancia de la clase Sale.
+$sale = new Sale(250000, date("Y-m-d"));
 
 // Asignación de valores a las propiedades total y date de la instancia $sale
-$sale->total = 250000;
-$sale->date = date("Y-M-d");
+/* ### Nota con los constructores ya no es necesario ### */ 
+//$sale->total = 250000;
+//$sale->date = date("Y-M-d");
 
-//Llamado del metodo.
+//Llamado del metodo de facturacion.
 $sale->createInvoice();
 
 //Llamado metodo propina
-$propina = $sale->createPropina(15);
+$propina = $sale->createTip(15000);
 echo $propina;
 
 echo "<br>";
@@ -25,12 +26,19 @@ print_r($sale);
     public $total;
     public $date;
 
+    /*Los constructores hacen que la creación de objetos sea más 
+    eficiente y estructurada.*/
+    public  function __construct($total, $date){
+      $this->total = $total;
+      $this->date = $date;
+    }
+
     //Funcion dentro de la clase: metodo
     public function createInvoice(){
       echo "Factura impresa";
     }
 
-    public function createPropina(){
+    public function createTip(){
        return;
     }
  }
