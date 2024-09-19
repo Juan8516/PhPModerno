19e38c689b68897152564fc9be003ab0670a77f6
Juan8516 <juan.ind85@gmail.com>
2024-09-19 14:29:13 -0500

    feat: uso metodos propios array

--- 03POO/01class.php
+++ 03POO/01class.php
@@ -9,10 +9,28 @@ $sale = new Sale();
 // Asignación de valores a las propiedades total y date de la instancia $sale
 $sale->total = 250000;
 $sale->date = date("Y-M-d");
+
+//Llamado del metodo.
+$sale->createInvoice();
+
+//Llamado metodo propina
+$propina = $sale->createPropina(15);
+echo $propina;
+
+echo "<br>";
 print_r($sale);
 
 // Creación de la clase Sale con propiedades públicas total y date
  class Sale {
     public $total;
     public $date;
+
+    //Funcion dentro de la clase: metodo
+    public function createInvoice(){
+      echo "Factura impresa";
+    }
+
+    public function createPropina(){
+       return;
+    }
  }
